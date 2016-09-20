27095f2e9cef6aff2ecfb7451a95ac60e4a31baa
Nik Nauwelaerts <Nik Nauwelaerts>
2016-09-20 14:10:50 +0200

    Login page + remembering creds

--- src/app/modules/welcome/welcome.component.ts
+++ src/app/modules/welcome/welcome.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  cred : {
+    userName: string;
+    passWord: string;
+    remember: boolean;
+  } = {userName : "", passWord:"", remember:true}
 
   constructor() { }
 
   ngOnInit() {
+    if(!!localStorage.getItem('cred')){
+      this.cred = JSON.parse(localStorage.getItem('cred'))
+    };
+  }
+
+  onSubmit() {
+    if (this.cred.remember){
+      localStorage.setItem('cred', JSON.stringify(this.cred));
+    }else{
+      localStorage.removeItem('cred');
+    }
+    
+    alert("yoho!");
   }
 
 }
